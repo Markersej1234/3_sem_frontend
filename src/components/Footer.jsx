decf4a6ba56bbc544f7722a74ff60599b0295b2f
Mark Gonzales Nielsen <cph-mn651@cphbusiness.dk>
2023-01-19 14:27:02 +0100

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer(props) {
+    return (
+        <footer className="footer">
+            &copy;2022 | CA2 Project - Timea Ballok
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ src/settings.js
@@ -0,0 +1,4 @@
+const URL = "http://localhost:8080/CA2_war_exploded";
+// const URL = "https://timeaballok.dk/tomcat/ca2_Timi";
+
+export default URL;
